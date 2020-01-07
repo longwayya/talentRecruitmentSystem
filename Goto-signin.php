754b0b0c00e28b5cb629ebb13ce637717ebe3ab6
Lynn <1245474940@qq.com>
2020-01-07 16:07:10 +0800

    修改了返回跳转，背景图片

--- Goto-signin.php
+++ Goto-signin.php
@@ -1,9 +1,9 @@
-<body style="background-image:url(E:\phpstudy2014\store\2.jpg);background-position:center;">
+<body style="background-image:url(2.jpg);background-position:center;">
  <meta charset="UTF-8">
  <title>注册</title>
  <style>       body{text-align:center}     </style>
-<a href="SignIn-personal.php"> 个人注册666</a>
-<a href="SignIn-company.php"> 企业注册888</a>
+<a href="SignIn-personal.php"> 个人注册</a>
+<a href="SignIn-company.php"> 企业注册</a>
 <br/><br/>
-<a href="Manager.php"> 返回</a>
+<a href="index.php"> 返回</a>
 </body>
