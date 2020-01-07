59a41f55bf5625479739f93044084bcca28967b3
Landy <1347673987@qq.com>
2020-01-07 10:57:05 +0800

    企业界面

---
+++ qiye.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(D:\php\PHPTutorial\WWW\shixun\2.jpg);background-position:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <h2>企业界面</h2>
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<a href="fabuzhaopin.php"> <font style="color:white">发布招聘</font></a>
+<a href="rencaisousuo.php"> <font style="color:white">人才搜索</font></a>
+<a href="qiye_M.php"> <font style="color:black">修改信息</font></a>
+<a href="Connect.php"> <font style="color:black">退出登录</font></a>
+<br><br/><br><br/>
+<a href="Q1.php"> <font style="color:white">大专毕业诚挚应聘教师岗位</font></a>
+<br><br/>
+<a href="Q2.php"> <font style="color:white">双一流高校应届毕业生诚挚应聘</font></a>
+<br><br/>
+<a href="Q3.php"> <font style="color:white">英语专业八级应聘翻译</font></a>
+<br><br/>
+<a href="Q4.php"> <font style="color:white">IT高科技人才，有丰富经验</font></a>
+<br><br/><br><br/>
+<a href="rencaisousuo.php"> <font style="color:black">查看更多应聘信息->></font></a>
+<br><br/>
+</form>
+</body>
