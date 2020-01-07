540864162507e454c68010c79914d7bd50719882
Lynn <1245474940@qq.com>
2020-01-07 14:54:32 +0800

    跳转至注册界面

---
+++ Goto-signin.php
@@ -0,0 +1,9 @@
+<body style="background-image:url(E:\phpstudy2014\store\2.jpg);background-position:center;">
+ <meta charset="UTF-8">
+ <title>注册</title>
+ <style>       body{text-align:center}     </style>
+<a href="SignIn-personal.php"> 个人注册</a>
+<a href="SignIn-company.php"> 企业注册</a>
+<br/><br/>
+<a href="Manager.php"> 返回</a>
+</body>
