a584af9e4be8978a93304275ec85a2e80d3770ef
Landy <1347673987@qq.com>
2020-01-07 15:34:33 +0800

    企业界面第二条广告消息

---
+++ Q2.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(D:\php\PHPTutorial\WWW\shixun\2.jpg);background-position:center;text-align:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <br></br>
+ <h2>双一流高校应届毕业生诚挚应聘</h2>
+  <br></br> 
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<font style="color:black">
+我是嘉兴学院临床医学院临床专业15级本科学生,将于2020年7月毕业。<br></br>
+在即将叩响社会这扇大门时,我怀着无比的真诚和高度责任感把我的人生向您靠拢。<br></br>
+五年的大学生活使我树立了正确的人生观，价值观,形成了热情、上进、 不屈不挠的性格和诚实、守信、有责任心、有爱心的人生信条。<br></br>
+五年,厚积薄发的五年,给我的”轻叩柴扉”留下一一个自信而又响亮的声音。<br></br>
+以上是我的求职面试自我介绍。<br></br>
+在即将走向社会之时,诚望贵单位全面考察我的情况,若能蒙您垂青,我将深感荣幸,并在日后工作中,不懈拼搏之劲,不失进取之心，克尽己能,为贵单位事业奉献自己的一-份力量。
+<br></br>相信在您们的支持下,我会和您所领导的单位- -样，直挂云帆济沧海,乘风破浪会有时!
+<br></br>
+联系方式：XXX
+<br></br><br></br>
+<a href="qiye.php"> <font style="color:white">返回首页</font></a>
+</form>
+</body>
