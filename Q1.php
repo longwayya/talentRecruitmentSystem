386de4d8f9fec13379fd7742fa2e87b50e5b1a45
Landy <1347673987@qq.com>
2020-01-07 15:27:34 +0800

    企业界面第一条广告消息

---
+++ Q1.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(D:\php\PHPTutorial\WWW\shixun\2.jpg);background-position:center;text-align:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <br></br>
+ <h2>大专毕业诚挚应聘教师岗位</h2>
+  <br></br> 
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<font style="color:black">本人大专毕业应届毕业生，在校期间考取了教师资格证，现诚挚应聘初中语文老师。<br></br>
+在校期间，我担任过学生干部，有良好的管理能力、组织能力、协调能力和强烈的集体主义精神，并且有实习经历。<br></br>
+我真诚希望给我个机会，我将在今后的工作中虚心学习、不断钻研，做好一名人民教师。<br></br>
+联系方式：XXX
+<br></br><br></br>
+<a href="qiye.php"> <font style="color:white">返回首页</font></a>
+</font>
+</form>
+</body>
