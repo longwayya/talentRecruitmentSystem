a2450e78fdc4ebd49770b68010d0f4ca8efc05af
Longway <5611282+howlongway@user.noreply.gitee.com>
2020-01-07 12:35:06 +0800

    Update DB

--- rencai.sql
+++ rencai.sql
@@ -1,15 +1,14 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version phpStudy 2014
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2020 年 01 月 06 日 09:04
+-- 生成日期: 2020 年 01 月 07 日 12:29
 -- 服务器版本: 5.5.53
 -- PHP 版本: 5.4.45
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -57,11 +56,21 @@ CREATE TABLE IF NOT EXISTS `manager` (
 
 CREATE TABLE IF NOT EXISTS `qiye` (
   `Qname` char(20) NOT NULL,
-  `Qtel` int(11) NOT NULL,
+  `Qtel` varchar(11) NOT NULL,
   `Qemail` char(20) NOT NULL,
   `Qpassword` char(20) NOT NULL,
-  `Qlocate` char(20) NOT NULL
+  `Qlocate` char(20) NOT NULL,
+  PRIMARY KEY (`Qname`),
+  UNIQUE KEY `Qname` (`Qname`),
+  UNIQUE KEY `Qname_2` (`Qname`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `qiye`
+--
+
+INSERT INTO `qiye` (`Qname`, `Qtel`, `Qemail`, `Qpassword`, `Qlocate`) VALUES
+('工商银行保定分行', '11111111111', '111@111.com', '123456', '保定莲池区');
 
 -- --------------------------------------------------------
 
