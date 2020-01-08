3eae4fba718b2bdd489e8ffbbdbd02394c6c5599
Landy <1347673987@qq.com>
2020-01-08 08:21:27 +0800

    个人界面

---
+++ geren.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(2.jpg);background-position:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <h2>个人界面</h2>
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<a href="geren_M.php"> <font style="color:white">修改简历</font></a>
+<a href="zhiweisousuo.php"> <font style="color:white">职位搜索</font></a>
+<a href="index.php"> <font style="color:black">退出登录</font></a>
+<br><br/><br><br/>
+<a href="R1.php"> <font style="color:white">高薪招聘房产销售人才</font></a>
+<br><br/>
+<a href="R2.php"> <font style="color:white">4S店高薪诚聘高学历人才</font></a>
+<br><br/>
+<a href="R3.php"> <font style="color:white">做良心地产中介招实力人才</font></a>
+<br><br/>
+<a href="R4.php"> <font style="color:white">安格置业诚聘人才</font></a>
+<br><br/><br><br/>
+<a href="zhiweisousuo.php"> <font style="color:black">查看更多招聘信息->></font></a>
+<br><br/>
+</form>
+</body>
