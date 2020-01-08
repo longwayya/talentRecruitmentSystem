1e0996a7703376d3d57020b95bb4e22d5e8862c9
Landy <1347673987@qq.com>
2020-01-08 10:26:04 +0800

    企业界面第三条广告消息

---
+++ Q3.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(D:\php\PHPTutorial\WWW\shixun\2.jpg);background-position:center;text-align:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <br></br>
+ <h2>英语专业八级应聘翻译</h2>
+  <br></br>
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<font style="color:black">
+尊敬的各位领导:你们好!<br></br>
+我是xx师范大学外语学院英语系xx级的一名学生，即将于明年七月完成本科学业，真正地步入社会，开始人生的新一段征程。<br></br>
+上大学后，我最郑重地告诉自己的一句话是:学通你的专业，利用它为你的人生开辟道路。<br></br>
+于是我以交际英语为方向，首先侧重于打好基础，从听、说、读、写几方面训练基本技能，在达到自如地运用英语的基础上，我涉猎了英美概况、英美文学、语言学等相关专业知识。<br></br>
+同时，我还通过了英语专业八级的考试。毕业后，我想做一名专业的翻译。<br></br>
+望各位领导给我个机会。<br></br>
+<br></br>
+联系方式：XXX
+<br></br><br></br>
+<a href="qiye.php"> <font style="color:white">返回首页</font></a>
+</font>
+</form>
+</body>
