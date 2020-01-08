632d49ac3be1404fc4c2396494bc25ead77ab14a
Landy <1347673987@qq.com>
2020-01-08 10:26:36 +0800

    企业界面第四条广告消息

---
+++ Q4.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(D:\php\PHPTutorial\WWW\shixun\2.jpg);background-position:center;text-align:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <br></br>
+ <h2>IT高科技人才，有丰富经验</h2>
+  <br></br> 
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<font style="color:black">尊敬的各位领导:你们好!<br></br>
+我是一名从事IT行业的工作人员，已有五年工作经历，在工作中积累了很多经验。<br></br>
+在专业知识上,我除了精通Visual Basic、SQL Server、ASP， 熟练使用Linux、Windows 9x/Me/NT/XX/XP 等操作系统，熟练使用Office、WPS办公自动化软件。<br></br>
+还自学HTML、Frontpage、Dreamweaver 、Fireworks 、Flash 等页制作相关软件。<br></br>
+对于常用软件都能熟练使用、运用自如。<br></br>
+望各位领导考虑一下，给我个提升自己的机会。<br></br>
+联系方式：XXX
+<br></br><br></br>
+<a href="qiye.php"> <font style="color:white">返回首页</font></a>
+</font>
+</form>
+</body>
