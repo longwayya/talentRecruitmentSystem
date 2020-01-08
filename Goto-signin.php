57339ab85d3ce1eeb58fbf6c7294a0cb46c11f95
Lynn <1245474940@qq.com>
2020-01-08 10:04:53 +0800

    修改界面

--- Goto-signin.php
+++ Goto-signin.php
@@ -1,9 +1,18 @@
 <body style="background-image:url(2.jpg);background-position:center;">
  <meta charset="UTF-8">
  <title>注册</title>
- <style>       body{text-align:center}     </style>
-<a href="SignIn-personal.php"> 个人注册</a>
-<a href="SignIn-company.php"> 企业注册</a>
+ <style>      body{background:#fff;font-size:18px;font-family:"Arial", "Tahoma", "微软雅黑", "雅黑";
+line-height:18px;padding:0px;margin:0px;text-align:center} 
+div{padding:18px} 
+img{border:0px;vertical-align:middle;padding:0;margin:0} 
+input, button{font-family:"Arial", "Tahoma", "微软雅黑", "雅黑";border:0;
+vertical-align:middle;margin:8px;line-height:18px;font-size:18px} 
+.btn{width:140px;height:36px;line-height:18px;font-size:18px;
+background:url("1.1.jpg") no-repeat left top;color:#FFF;padding-bottom:4px}  </style>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<a href="SignIn-personal.php"> <input class="btn" type="button" value="个人注册"></a>
+<a href="SignIn-company.php"> <input class="btn" type="button" value="企业注册"></a>
 <br/><br/>
-<a href="index.php"> 返回</a>
+<a href="index.php"> <input class="btn" type="button" value="返回"></a>
 </body>
+
