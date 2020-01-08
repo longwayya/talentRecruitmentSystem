a31162822900aab91509a66387809af56abbf367
Landy <1347673987@qq.com>
2020-01-08 14:54:51 +0800

    更改修改简历跳转链接

--- geren.php
+++ geren.php
@@ -7,7 +7,7 @@ session_start();
  <style>       body{text-align:center}     </style>
  <h2>个人界面</h2>
 <form action="" method="POST" name="myform" onsubmit="return check()">
-<a href="update 2.0.php"> <font style="color:white">修改简历</font></a>
+<a href="update 3.0.php"> <font style="color:white">修改简历</font></a>
 <a href="search_J.php"> <font style="color:white">职位搜索</font></a>
 <a href="index.php"> <font style="color:black">退出登录</font></a>
 <br><br/><br><br/>
