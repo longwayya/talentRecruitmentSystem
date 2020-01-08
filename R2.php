cc2f531dd0146b2464a50955234cc6dbbdcd61fb
Landy <1347673987@qq.com>
2020-01-08 15:34:09 +0800

    人才界面第二条广告消息

---
+++ R2.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<body style="background-image:url(2.jpg);background-position:center;text-align:center;">
+ <meta charset="UTF-8">
+ <title>人才中介网</title>
+ <style>       body{text-align:center}     </style>
+ <h2>4S店高薪诚聘高学历人才</h2>
+<form action="" method="POST" name="myform" onsubmit="return check()">
+<font style="color:black">
+要求：<br></br>
+1、本科及以上学历，男女不限，21-30岁;<br></br>
+2、性格开朗，普通话标准，具备强烈的成功欲望和对个人价值实现的追求;<br></br>
+3、具备半年以上销售工作经验及一定的培训或相关岗位工作经验。<br></br>
+薪资待遇：<br></br>
+1、试用期(3个月)：3000元/月;<br></br>
+2、转正：8000-10000元/月。<br></br>
+工作地点：<br></br>
+北京市XX公司<br></br>
+联系方式：<br></br>
+王小姐：XXX
+<br></br>
+<a href="geren.php"> <font style="color:white">返回首页</font></a>
+</font>
+</form>
+</body>
