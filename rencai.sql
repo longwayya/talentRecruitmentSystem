e72bba214c5dfeee8edad76230f26f08cd5a7a36
Landy <1347673987@qq.com>
2020-01-09 17:09:16 +0800

    修改数据库

--- rencai.sql
+++ rencai.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2020 年 01 月 08 日 10:21
+-- 生成日期: 2020 年 01 月 09 日 09:07
 -- 服务器版本: 5.5.53
 -- PHP 版本: 5.4.45
 
@@ -34,9 +34,7 @@ CREATE TABLE IF NOT EXISTS `geren` (
   `Gyear` int(6) NOT NULL,
   `Gxueli` char(10) NOT NULL,
   `Gemail` char(20) NOT NULL,
-  `Gzhuanye` char(10) NOT NULL,
-  PRIMARY KEY (`Gname`),
-  UNIQUE KEY `Gname` (`Gname`)
+  `Gzhuanye` char(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -44,21 +42,10 @@ CREATE TABLE IF NOT EXISTS `geren` (
 --
 
 INSERT INTO `geren` (`Gname`, `Gpassword`, `Gtel`, `Gsex`, `Gyear`, `Gxueli`, `Gemail`, `Gzhuanye`) VALUES
-('张三', '123456', '123456', '男', 2020, '本科', '111@qq.com', '信息安全'),
-('李四', '123456', '2222', '女', 2019, '专科', '2.@。com', '无'),
-('王五', '123456', '123456', '女', 2019, '硕士', '2000@.com', '法学'),
-('赵四', '123456', '123456', '女', 2019, '本科', '111@qq.com', '法学');
-
--- --------------------------------------------------------
-
---
--- 表的结构 `manager`
---
-
-CREATE TABLE IF NOT EXISTS `manager` (
-  `Mname` char(20) NOT NULL,
-  `Mpassword` char(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+('王五', '003', '333333', '男', 22, '本科', '2345@qq.com', '文学'),
+('李四', '002', '222222', '女', 22, '研究生', '5678@qq.com', '英语'),
+('张三', '001', '111111', '男', 20, '本科', '1234@qq.com', '计算机'),
+('王五', '111', '2', '2', 2, '2', '2', '2');
 
 -- --------------------------------------------------------
 
@@ -68,13 +55,10 @@ CREATE TABLE IF NOT EXISTS `manager` (
 
 CREATE TABLE IF NOT EXISTS `qiye` (
   `Qname` char(20) NOT NULL,
-  `Qtel` varchar(11) NOT NULL,
+  `Qtel` int(11) NOT NULL,
   `Qemail` char(20) NOT NULL,
   `Qpassword` char(20) NOT NULL,
-  `Qlocate` char(20) NOT NULL,
-  PRIMARY KEY (`Qname`),
-  UNIQUE KEY `Qname` (`Qname`),
-  UNIQUE KEY `Qname_2` (`Qname`)
+  `Qlocate` char(20) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -82,7 +66,9 @@ CREATE TABLE IF NOT EXISTS `qiye` (
 --
 
 INSERT INTO `qiye` (`Qname`, `Qtel`, `Qemail`, `Qpassword`, `Qlocate`) VALUES
-('工商银行保定分行', '11111111', '11111', '123456', '11111');
+('中国电信', 3, '0003@163.com', '0003', '石家庄'),
+('中国联通', 2, '0002@163.com', '0002', '保定'),
+('中国移动', 1, '0001@163.com', '0001', '北京');
 
 -- --------------------------------------------------------
 
@@ -94,10 +80,7 @@ CREATE TABLE IF NOT EXISTS `zhiwei` (
   `Zname` char(20) NOT NULL,
   `Zqiye` char(20) NOT NULL,
   `Znumber` int(5) NOT NULL,
-  `Zmoney` int(10) NOT NULL,
-  KEY `Zqiye` (`Zqiye`),
-  KEY `Zqiye_2` (`Zqiye`),
-  KEY `Zqiye_3` (`Zqiye`)
+  `Zmoney` int(10) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -105,9 +88,10 @@ CREATE TABLE IF NOT EXISTS `zhiwei` (
 --
 
 INSERT INTO `zhiwei` (`Zname`, `Zqiye`, `Znumber`, `Zmoney`) VALUES
-('文员', '工商银行保定分行', 2, 2000),
-('前台', '工商银行保定分行', 2, 2000),
-('销售', '工商银行保定分行', 2, 2000);
+('客服', '中国移动', 2, 5000),
+('维修人员', '中国联通', 1, 8000),
+('客服', '中国联通', 2, 4000),
+('前台', '中国电信', 1, 3000);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
